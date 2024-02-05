05d7027aa6efc4b262584b81bafeaa44ea972b07
chiragaiml21 <chirag.gla_cs.aiml21@gla.ac.in>
2024-02-05 16:13:24 +0530

    demo

---
+++ models/todos/sub_todo.models.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+
+const subTodoSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    complete: {
+        type: Boolean,
+        default: false
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.
+    }
+}, { timestamps: true })
+
+export const SubTodo = mongoose.model('SubTodo', subTodoSchema)
